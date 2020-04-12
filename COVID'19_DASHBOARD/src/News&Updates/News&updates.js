17cfd5e1c5a9eeb70ecc30e53135ae4d31bb5f99
deepalisrivas <deepali.srivastava@tothenew.com>
2020-04-13 00:50:29 +0530

    component created

--- COVID'19_DASHBOARD/src/News&Updates/News&updates.js
+++ COVID'19_DASHBOARD/src/News&Updates/News&updates.js
@@ -1,16 +1,22 @@
 import React, {Component} from 'react';
 import './News&updates.css';
 import illustrator from '../../assets/Illustration.svg';
+import arrow from '../../assets/Arrow.svg';
 
 class News extends Component{
+
+
 render(){
     return(
         <div className="wrapper1">
-            <div className="illustrator-img"><img src={illustrator}/></div>
+            <div className="illustrator-img"><img src={illustrator} class="img-illustrate"/></div>
             <div className="news-content">
-                <div className="news-box"><h3>News & Updates</h3></div>
+                <div className="news-box"><h4 class="h4">News & Updates</h4></div>
                 <div className="news-symptoms"><h3>5 Symptoms of Corona Virus that you should know</h3></div>
-                <div className="news-slider"></div>
+                <div className="news-slider"><span>Read More</span><button onClick={this.props.func} class="btn-arrow"><img src={arrow} alt="Read More"/></button></div>
+
+                 
+           
             </div>
         </div>
     )
