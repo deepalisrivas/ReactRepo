148252f7b0dadf2a7b64c808eaae105434653803
deepalisrivas <deepali.srivastava@tothenew.com>
2020-04-15 13:22:12 +0530

    recreating line-chart

--- COVID'19_DASHBOARD/src/Home/Charts/Charts.js
+++ COVID'19_DASHBOARD/src/Home/Charts/Charts.js
@@ -2,26 +2,14 @@ import React , {Component} from 'react';
 import './Charts.css';
 import Line from './Line-chart';
 import axios from 'axios'
-import { useParams } from 'react-router-dom';
+import LineChart from 'react-linechart';
+// import '../node_modules/react-linechart/dist/styles.css';
+
+// import { useParams } from 'react-router-dom';
 
  class Charts extends Component{
 
-    createFakeData(param){
-        // This function creates data that doesn't look entirely random
-        const data = []
     
-        for (let x = 0; x <= 30; x++) {
-          const random = Math.random();
-          const temp = data.length > 0 ? data[data.length-1].y : 50;
-          const y = random >= .45 ? temp + Math.floor(random * 20) : temp - Math.floor(random * 20);
-          data.push({x,y})
-        }
-        // console.log(data)
-
-        return data;
-        
-      };
-
       
 
     
@@ -36,65 +24,71 @@ import { useParams } from 'react-router-dom';
     //   }
 
      state = {
-        data : [
-            {date : ''},
-            {recovered : ''}
-        ],
-        
-    
+        Cases : {
+            date : [],
+            recovered : []
+        }
      }
 
     componentDidMount(){
         axios.get('https://api.covid19api.com/total/dayone/country/India')
        
         .then(response => {
-            debugger
+            
             let tempDate = [];
-            // let tempData = [];
-            // let newObject = [];
-            // let tempRecovered = []
-
+            let tempRecovered =[];
+//             
             for(var i = 0;i < response.data.length; i++){
-                // const x = response.data[i].Date;
-                // const y = response.data[i].Confirmed
+//                
+           tempDate.push(response.data[i].Date);
+                tempRecovered.push(response.data[i].Confirmed);
+//             // console.log(tempData);
+//             //  tempData = Object.values(tempDate);
+            // tempDate.push({date : response.data[i].Date, recovered : response.data[i].Confirmed})
 
-               
-            tempDate.push({date : response.data[i].Date, recovered : response.data[i].Confirmed});
-            // console.log(tempData);
-            //  tempData = Object.values(tempDate);
-            // this.setState({date : tempDate})
+            // this.setState({Cases : tempDate})
             // this.state.data.push({date : response.data[i].Date, recovered : response.data[i].Confirmed})
-            this.setState({...this.state,date:tempDate})
-                 
-                // console.log(newObject)
-            // tempRecovered.push()
-            // const data = response.data[i].Date
-            // this.setState({data:tempDate})
-            // this.setState({recovered:tempRecovered})
-            }
-        
-            // console.log(this.state.data)
-            // const name = this.state.data.map(item => {item.this.state.data.date});
-            // this.setState({name})
-            // console.log(name)
-            // debugger
-            // newObject = ;
-            // console.log(Object.entries(this.state.data).map((e) => ( {[[]]:e[1] } )))
-
-            // return data;
-            // console.log(this.state.data)
-        })
-        // console.log(this.state.data.map(item =>(key=item.id)))
-                
- }
+            this.setState({Cases:{
+                            date:tempDate,
+                            recovered:tempRecovered}
+                         })
+                    // console.log(this.state);
+                }
+                // console.log(this.state.Cases.recovered)
  
+            })
+        }
+//     
+        render() {
+console.log(this.state.Cases.recovered)  
 
-    render(){
-        
-        console.log(this.state.data)
 
-        // const name = this.state.data
-        
+
+            const data = [
+                {									
+                    color: "red", 
+                    points: [{x: 1, y: 2}, {x: 3, y: 5}, {x: 7, y: -3}]
+                    // points : [{x:1 , y:this.state.Cases.recovered[0]},
+                    // {x:2 , y:this.state.Cases.recovered[1]},
+                    //     {x:3 , y:this.state.Cases.recovered[2]},
+                    //         {x:4 , y:this.state.Cases.recovered[3]}
+                    //             ]
+                }
+
+            ];
+            
+        // console.log(data)
+            // console.log(this.state.Cases.recovered[0])
+             
+            //         // points :this.state.Cases
+            //         points : [{x:1 , y:this.state.Cases.recovered[0]},
+            //         {x:2 , y:this.state.Cases.recovered[1]},
+            //             {x:3 , y:this.state.Cases.recovered[2]},
+            //                 {x:4 , y:this.state.Cases.recovered[3]}
+            //                     ]
+
+       
+    
 
         return(
             <div className="wrapper">
@@ -108,9 +102,14 @@ import { useParams } from 'react-router-dom';
                 </div>
                 <div>
                    
-                    <Line data={this.createFakeData()}/>
+                    {/*<Line data={this.createFakeData()}/>*/}
                     
                       <div>
+                      <LineChart 
+                            width={400}
+                            height={300}
+                            data={data}
+                             />
                        {/*{this.state.data.map(postItem => (
                         <div key={postItem.id}>
                             <h1>{postItem.title}</h1>
@@ -119,6 +118,7 @@ import { useParams } from 'react-router-dom';
                         <h1>{postItem.recovered}</h1>
                         </div>
                        )}*/}
+                       
                         
                         </div>
                        
