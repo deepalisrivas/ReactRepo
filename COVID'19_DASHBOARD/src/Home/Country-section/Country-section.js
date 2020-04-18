095c4e7473f5596409949df2e30e83cd828b427d
deepalisrivas <deepali.srivastava@tothenew.com>
2020-04-18 17:53:49 +0530

    search_bar added, Code compact

--- COVID'19_DASHBOARD/src/Home/Country-section/Country-section.js
+++ COVID'19_DASHBOARD/src/Home/Country-section/Country-section.js
@@ -16,106 +16,59 @@ import up from '../../../assets/Up.svg';
 
 class CountrySection extends Component{
     state = {
-        USA : [],
-        UK : [],
-        INDIA : [],
-        ITALY : [],
-        SPAIN : [],
-        CHINA : [],
-        TURKEY : [],
-        IRAN : [],
-        SWITZERLAND : [],
-        GERMANY : []
-        // cases : "",
-        // recovered : "",
+        countries: [],
+        search:null
     }
     
+    searchSpace=(event)=>{
+        let keyword = event.target.value;
+        this.setState({search:keyword})
+      }
 
     componentDidMount(){
-        axios.get('https://corona.lmao.ninja/countries?sort=country')
+        axios.get('https://api.covid19api.com/summary')
         .then(response => {
-            // console.log()
-            // let data = response.data;
-            // for(let id = 0; id<response.data.length; id++){
-            //     if(data[id].country==="USA"){
-            //         var cases = (data[id].cases);
-            //     }
-            // }
-            const USA = response.data[9];
-            this.setState({USA});
-            const UK = response.data[10];
-            this.setState({UK});
-            const ITALY = response.data[111];
-            this.setState({ITALY});
-            const CHINA = response.data[168];
-            this.setState({CHINA});
-            const TURKEY = response.data[13];
-            this.setState({TURKEY});
-            const GERMANY = response.data[9];
-            this.setState({GERMANY});
-            const IRAN = response.data[116];
-            this.setState({IRAN});
-            const INDIA = response.data[118];
-            this.setState({INDIA});
-            const SWITZERLAND = response.data[22];
-            this.setState({SWITZERLAND});
-            const SPAIN = response.data[29];
-            this.setState({SPAIN});
-
-            // for(let i=0;i<Affected.lenth;i++){
-            //     const arr = Affected[i];
+            const countries = response.data.Countries
+            this.setState({countries});
             
-            // }
-            // for(let i in Affected){
-            //     if(i.country==="USA"){
-            //         console.log(i.country.cases);
-            //     }
-            // }
-            
-            
-            // console.log(response.data);
         })
+        .catch(error=>{"Something went wrong!"})
     }
 
    
     render(){
+        const countriesImage = [usa,uk,italy,china,spain,germany,iran,india,switzerland,turkey]
+        const data = this.state.countries.filter((item)=>
+          {
+            if(this.state.search == null)
+                return item
+            else if(item.Country.toLowerCase().includes(this.state.search.toLowerCase()))
+            {
+                return item
+            }
+          }) .map((item,index)=>
+          {
+            
+        return(
+             <div className="country-block" key={item.Country}>
+                <div className="sub-country-block">
+                         <img src={countriesImage[index]} className="flags" alt={item.Country} />
+                         <h5>{item.Country}</h5>
+                    <div className="arrow"><img src={up} alt="danger"/></div>
+                    
+                </div>
+                <div className="country-data" >{item.TotalConfirmed} Affected {item.TotalRecovered} Recovered</div>
+             </div>
+        )})
         return (
             
                 <div className="content">
                 
                 <div className="scrollbar" id="style-3">
-    
-                     <div className="country-block"><div className="sub-country-block"><img src={usa} className="flags" alt="USA"/><h5>United States</h5><div className="arrow"><img src={up} alt="danger"/></div></div>
-                     <div className="country-data">{this.state.USA.cases} Affected {this.state.USA.recovered} Recovered</div>
-                     </div>
-                     <div className="country-block"><div className="sub-country-block"><img src={uk} className="flags" alt="UK"/><h5>United Kingdom</h5><div className="arrow"><img src={up} alt="danger"/></div></div>
-                     <div className="country-data">{this.state.UK.cases} Affected {this.state.UK.recovered} Recovered</div>
-                     </div>
-                     <div className="country-block"><div className="sub-country-block"><img src={italy} className="flags" alt="ITALY"/><h5>Italy</h5><div className="arrow"><img src={up} alt="danger"/></div></div>
-                     <div className="country-data">{this.state.ITALY.cases} Affected {this.state.ITALY.recovered} Recovered</div>
-                     </div>
-                     <div className="country-block"><div className="sub-country-block"><img src={china} className="flags" alt="CHINA"/><h5>China</h5><div className="arrow"><img src={up} alt="danger"/></div></div>
-                     <div className="country-data">{this.state.CHINA.cases} Affected {this.state.CHINA.recovered} Recovered</div>
-                     </div>
-                     <div className="country-block"><div className="sub-country-block"><img src={spain} className="flags" alt="SPAIN"/><h5>Spain</h5><div className="arrow"><img src={up} alt="danger"/></div></div>
-                     <div className="country-data">{this.state.SPAIN.cases} Affected {this.state.SPAIN.recovered} Recovered</div>
-                     </div>
-                     <div className="country-block"><div className="sub-country-block"><img src={germany} className="flags" alt="GERMANY"/><h5> Germany</h5><div className="arrow"><img src={up} alt="danger"/></div></div>
-                     <div className="country-data">{this.state.GERMANY.cases} Affected {this.state.GERMANY.recovered} Recovered</div>
-                     </div>
-                     <div className="country-block"><div className="sub-country-block"><img src={iran} className="flags" alt="IRAN"/><h5>Iran</h5><div className="arrow"><img src={up} alt="danger"/></div></div>
-                     <div className="country-data">{this.state.IRAN.cases} Affected {this.state.IRAN.recovered} Recovered</div>
-                     </div>
-                     <div className="country-block"><div className="sub-country-block"><img src={india} className="flags" alt="INDIA"/><h5>India</h5><div className="arrow"><img src={up} alt="danger"/></div></div>
-                     <div className="country-data">{this.state.INDIA.cases} Affected {this.state.INDIA.recovered} Recovered</div>
-                     </div>
-                     <div className="country-block"><div className="sub-country-block"><img src={switzerland} className="flags" alt="SWITZERLAND"/><h5>Switzerland</h5><div className="arrow"><img src={up} alt="danger"/></div></div>
-                     <div className="country-data">{this.state.SWITZERLAND.cases} Affected {this.state.SWITZERLAND.recovered} Recovered</div>
-                     </div>
-                     <div className="country-block"><div className="sub-country-block"><img src={turkey} className="flags" alt="TURKEY"/><h5>Turkey</h5><div className="arrow"><img src={up} alt="danger"/></div></div>
-                     <div className="country-data">{this.state.TURKEY.cases} Affected {this.state.TURKEY.recovered} Recovered</div>
-                     </div>
                 
+                <input type="text" placeholder="Search Location" class="searchbar" onChange={(e)=>this.searchSpace(e)} />
+                     {data}
+                    
                      
                      </div>
                 </div>
